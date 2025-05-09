3f3fb768c163ce4079d4cb508e7d70fb88bb4f4f
Volodymyr Pavlenko <solaryasha@gmail.com>
2025-05-09 17:20:45 -0400

    fix linter errors

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -10,7 +10,7 @@ export default function NotFound() {
             Page Not Found
           </h2>
           <p className="mt-2 text-sm text-gray-600">
-            The page you're looking for doesn't exist or has been moved.
+            The page you&apos;re looking for doesn&apos;t exist or has been moved.
           </p>
         </div>
         <div>
